85900c7b05d06a1841fc492278309494d1ca4ab5
4rkaitz <arkaitz.garot@gmail.com>
2020-10-07 12:19:16 +0200

    Estructura actualizada.

--- app/src/main/java/com/example/ayashome/MainActivity.java
+++ app/src/main/java/com/example/ayashome/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.ayashome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    FrameLayout listaServicios;
+    private Toolbar MainToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listaServicios = findViewById(R.id.frameLayoutList);
+
+        final Toolbar MainToolbar = findViewById(R.id.toolbarMain);
+        MainToolbar.setTitle("Aya's Home");
+
+
     }
 }
