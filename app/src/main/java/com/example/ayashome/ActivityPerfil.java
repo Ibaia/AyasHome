a114531d11c34cd1365a7a1c6251d09d1bd5ac7e
argyll8 <sammyls@protonmail.com>
2020-10-15 11:22:19 +0200

    LogOut

--- app/src/main/java/com/example/ayashome/ActivityPerfil.java
+++ app/src/main/java/com/example/ayashome/ActivityPerfil.java
@@ -1,14 +1,48 @@
 package com.example.ayashome;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import static com.example.ayashome.MainActivity.mGoogleSignInClient;
 
 public class ActivityPerfil extends AppCompatActivity {
+
+    Button logOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        logOut = findViewById(R.id.button);
+
+        logOut.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                signOut();
+            }
+        });
+    }
+
+    private void signOut()
+    {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent goBack = new Intent(ActivityPerfil.this, MainActivity.class);
+                        startActivity(goBack);
+                    }
+                });
     }
 }
