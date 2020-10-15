7f8a0e2d41d639862191a021f284cc9bd7838bd5
argyll8 <sammyls@protonmail.com>
2020-10-15 10:14:23 +0200

    LogIN

--- app/src/main/java/com/example/ayashome/MainActivity.java
+++ app/src/main/java/com/example/ayashome/MainActivity.java
@@ -7,7 +7,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -16,15 +18,31 @@ import android.widget.Toolbar;
 import com.example.ayashome.adapter.MainRecyclerAdapter;
 import com.example.ayashome.model.ItemsServicios;
 import com.example.ayashome.model.Servicios;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.ayashome.Values.*;
 
 public class MainActivity extends AppCompatActivity {
 
     FrameLayout listaServicios;
     private Toolbar mainToolbar;
     private ImageView fotoPerfil;
+
+    SignInButton gButton;
+    GoogleSignInClient mGoogleSignInClient;
+    FirebaseFirestore db;
 
     RecyclerView rvVertical;
     MainRecyclerAdapter mainRecyclerAdapter;
@@ -34,14 +52,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fotoPerfil = findViewById(R.id.imgPerfil);
+        db = FirebaseFirestore.getInstance();
 
+        fotoPerfil = findViewById(R.id.imgPerfil);
         fotoPerfil.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intentPerfil = new Intent(MainActivity.this, ActivityPerfil.class);
-                startActivity(intentPerfil);
+                GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(MainActivity.this);
+                if(account == null)
+                {
+                    signIn();
+                }
+                else
+                {
+                    goReservas();
+                }
             }
         });
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         // estupidos datos de prueba
 
@@ -105,4 +138,64 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void signIn()
+    {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN)
+        {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            //updateUI(account);
+            goReservas();
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+
+    private void goReservas()
+    {
+        Intent intentPerfil = new Intent(MainActivity.this, ActivityPerfil.class);
+        startActivity(intentPerfil);
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        //updateUI(account);
+    }
+
+/*    private void updateUI(GoogleSignInAccount account)
+    {
+        if(account != null)
+        {
+            fotoPerfil.setImageDrawable();
+        }
+        else
+        {
+            Drawable img = new Drawable()
+            fotoPerfil.setImageDrawable();
+        }
+    }*/
 }
