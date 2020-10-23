08a1d03ebb02b1e3fbb826adb670f36f563d0165
4rkaitz <arkaitz.garot@gmail.com>
2020-10-23 12:13:23 +0200

    for para deshabilitar lunes

--- app/src/main/java/com/dosdmtres/ayashome/Datos.java
+++ app/src/main/java/com/dosdmtres/ayashome/Datos.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -12,15 +14,18 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
-import java.time.Year;
+import com.google.type.DayOfWeek;
+
 import java.util.Calendar;
 
 
 public class Datos extends AppCompatActivity {
 
 
+    private static final String TAG = null;
     private ImageView imageView;
     private TextView servicio;
     private TextView descripcion;
@@ -32,6 +37,10 @@ public class Datos extends AppCompatActivity {
     private EditText fecha;
     private EditText hora;
     private int numHora, minuto,  dia, mes, ano;
+    private boolean formularioCorrecto = true;
+    private boolean formFecha = false;
+    private boolean formHora = false;
+    private boolean formularioCompleto = false;
 
 
     @Override
@@ -70,6 +79,7 @@ public class Datos extends AppCompatActivity {
         mes = calendario.get(Calendar.MONTH);
         ano = calendario.get(Calendar.YEAR);
 
+
         DatePickerDialog dp = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
@@ -79,22 +89,49 @@ public class Datos extends AppCompatActivity {
                     a = dayOfMonth + "/" + "0" + (month + 1) + "/" + year;
                 }
                 fecha.setText(a, TextView.BufferType.EDITABLE);
+
+                //If the selected day is monday, show a warning
+                Calendar max = Calendar.getInstance();
+                max.add(Calendar.YEAR, 1);
+
+                //takes every day, one by one with a for
+                for (Calendar contador = Calendar.getInstance(); contador.before(max); contador.add(Calendar.DATE, 1)) {
+                    //takes the day of the for
+                    int fechaSeleccionada = contador.get(Calendar.DAY_OF_WEEK);
+
+                    //for each day it takes, checks if the selected day is monday, if it is, sends a toast and block the "reservar" button
+                    if (fechaSeleccionada == Calendar.MONDAY) {
+                        /*Calendar[] disabledDays =  new Calendar[1];
+                        disabledDays[0] = contador;*/
+                        Log.d(TAG, "esto es" + fechaSeleccionada);
+
+                        if(dayOfMonth == fechaSeleccionada) {
+                            CharSequence text = "Por favor, seleccione un día que no sea lunes.";
+                            Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+
+                            toast.show();
+
+                            formularioCorrecto = false;
+                        }else {
+                            formFecha = true;
+                        }
+                    }
+                }
+
             }
         }, ano, mes, dia);
         //Setting min date to the current date
         dp.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
-        dp.setTitle("");
 
-        for(Calendar contador = Calendar.getInstance(); contador.before(contador.YEAR+2); contador = Calendar.getInstance()) {
-            int dayOfWeek = contador.get(Calendar.DAY_OF_WEEK);
-            if (dayOfWeek == Calendar.TUESDAY) {
-                Calendar[] disabledDays = new Calendar[1];
-                disabledDays[0] = contador;
-                dp.setDisabledDays(disabledDays);
-            }
-        }
+        // Setting Max Date to next 2 years
+       // dp.getDatePicker().setMaxDate(Calendar.YEAR + 1);
 
+
+        dp.setTitle("Seleccionar fecha");
         dp.show();
+
+        comprobar();
+
     }
 
     public void mostrarHora() {
@@ -110,9 +147,23 @@ public class Datos extends AppCompatActivity {
                     a = hourOfDay + ":" + "0" + minute;
                 }
                 hora.setText(a, TextView.BufferType.EDITABLE);
+
             }
         }, numHora, minuto, false);
         tp.show();
+
+        formHora = true;
+        comprobar();
+    }
+
+    //check if the form is completed and correct
+    public void comprobar() {
+        if(formFecha && formHora) {
+            formularioCompleto = true;
+        }
+        if(!formularioCorrecto && !formularioCompleto) {
+            reserva.setEnabled(false);
+        }
     }
 
 }
