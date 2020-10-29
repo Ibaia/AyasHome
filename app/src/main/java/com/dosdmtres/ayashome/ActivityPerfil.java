87546ec0f02d3f1d224a38ca061d242015a6680f
JaviVaca <vacallona@gmail.com>
2020-10-29 14:03:33 +0100

    a falta de ordenar y comentar codigo. funcional

--- app/src/main/java/com/dosdmtres/ayashome/ActivityPerfil.java
+++ app/src/main/java/com/dosdmtres/ayashome/ActivityPerfil.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -23,6 +24,7 @@ import static com.dosdmtres.ayashome.MainActivity.mGoogleSignInClient;
 public class ActivityPerfil extends AppCompatActivity {
 
     Button logOut;
+    Button btnHome;
     ListView list;
     public static ReservationAdapter rAdapter1;
     TextView thisUser;
@@ -37,6 +39,17 @@ public class ActivityPerfil extends AppCompatActivity {
         list = findViewById(R.id.rList);
         thisUser = findViewById(R.id.thisUser);
         noR = findViewById(R.id.noReservations);
+        btnHome = findViewById(R.id.btnHome);
+
+        //Push the logo to go back
+        btnHome.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                MainActivity.goHome(ActivityPerfil.this);
+            }
+        });
 
         String email = (String)getIntent().getSerializableExtra("USER");
 
